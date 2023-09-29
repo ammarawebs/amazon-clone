af2b43dfd27cee1a147501e73aff8e6d9640012a
ammarawebs <ammar.awebs@gmail.com>
2023-09-29 20:48:29 +0500

    latest dependencies

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AppProvider } from './context/ProductContext';
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-// <Router>
-//     <AppProvider>
-    
-//     <App />
-    
-//     </AppProvider>
-//     </Router>
-
-//   </React.StrictMode>
-// );
-
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-  <Router basename="/" hashType="noslash">
-  <AppProvider>
-    <App/>
+<Router>
+    <AppProvider>
+    
+    <App />
+    
     </AppProvider>
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+
+  </React.StrictMode>
 );
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//   <Router basename="/" hashType="noslash">
+//   <AppProvider>
+//     <App/>
+//     </AppProvider>
+//     </Router>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
